13b1df1ad63b92b82b4dd986755f6ffe593e6523
NameIsA <nameisa@prodigymathwiki.com>
2021-02-27 06:58:25 -0800

    Updating member count

--- assets/templates/vars.php
+++ assets/templates/vars.php
@@ -1,7 +1,7 @@
 <?php
 $digitalTealConfig = array(
-    "about" => "DigitalTeal is a coding group consisting of 4 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound.",
+    "about" => "DigitalTeal is a coding group consisting of 5 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound.",
     "aboutHeader" => "About DigitalTeal",
-    "metaDescription" => "DigitalTeal is a coding group consisting of 4 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound."
+    "metaDescription" => "DigitalTeal is a coding group consisting of 5 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound."
 );
 ?>
