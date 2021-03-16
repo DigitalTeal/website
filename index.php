3f6fb37c6346405bd85b0be72a49de30d3ddcada
ChickenOnYT <nameisa@prodigymathwiki.com>
2021-03-16 11:46:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,15 @@
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#008080">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap");
         body {
@@ -46,10 +55,26 @@
         />
         <!-- Description -->
         <section id="description" class="w3-container w3-center w3-content" style="max-width: 600px;">
-            <h2 class="w3-wide">DigitalTeal</h2>
-            <p class="w3-opacity"><i>DigitalTeal is a small coding group</i></p>
-            <p class="w3-justify">
-                 <div class="col-sm-8 tabcontent" id="info" style="display: block;">
+             <div class="container" style="margin-top: 30px;">
+            <div class="row">
+                <h1>DigitalTeal</h1>
+                <div class="col-sm-4">
+                    <h1><?php echo $digitalTealConfig["aboutHeader"]; ?></h1>
+                    <p><?php echo $digitalTealConfig["about"]; ?></p>
+                    <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#info" onclick="openTab(event, 'info');" id="defaultOpen">Info</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#jumpbound" onclick="openTab(event, 'jumpbound');" id="jumpboundOpen">JumpBound</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#updates" onclick="openTab(event, 'updates');" id="updatesOpen">Latest updates</a>
+                        </li>
+                    </ul>
+                    <hr class="d-sm-none" />
+                </div>
+                <div class="col-sm-8 tabcontent" id="info" style="display: block;">
                     <noscript>JavaScript is required for this page to display properly.</noscript>
                     <h2>General information</h2>
                     <?php echo $digitalTealConfig["about"]; ?>
@@ -73,13 +98,14 @@
                     <a class="twitter-timeline" data-width="300" data-height="300" data-dnt="true" href="https://twitter.com/digital_teal?ref_src=twsrc%5Etfw">Tweets by digital_teal</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     <div class="fb-page" data-href="https://www.facebook.com/digitalteal" data-tabs="timeline" data-width="300" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/digitalteal" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/digitalteal">DigitalTeal</a></blockquote></div>
                 </div>
-            </p>
+            </div>
+        </div>
         </section>
         <!-- Footer -->
         <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
-            <a href="#"><i class="fa fa-facebook-official"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="https://facebook.com/DigitalTeal"><i class="fa fa-facebook-official"></i></a>
+            <a href="https://instagram.com/digital_teal"><i class="fa fa-instagram"></i></a>
+            <a href="https://twitter.com/digital_teal"><i class="fa fa-twitter"></i></a>
         </footer>
         
         
