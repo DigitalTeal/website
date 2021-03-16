1101f82aa1f28dda0bfc333e95ef5006126a0645
ChickenOnYT <nameisa@prodigymathwiki.com>
2021-03-16 09:43:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<?php
-require ("assets/templates/vars.php");
-?>
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>DigitalTeal - Home page</title>
-        <meta name="description" content="<?php echo $digitalTealConfig["metaDescription"]; ?>"/>
+<html>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="/w3css/3/w3.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
+    <title>DigitalTeal - Home page</title>
+        <meta name="description" content="DigitalTeal is a coding group consisting of 5 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound."/>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="canonical" href="https://www.digitalteal.com/" />
-        
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -17,61 +16,40 @@ require ("assets/templates/vars.php");
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#008080">
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="/assets/css/style.css" />
-
-<?php include("assets/templates/analyticsCode.html"); ?>
-    </head>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap");
+        body {
+            font-family: Rubik;
+            background-color: #701c3c;
+        }
+        #description {
+        	background-color: #701c3c;
+            color: white;
+        }
+    </style>
     <body>
-        <div id="fb-root"></div>
-        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="oG4OyHff"></script>
-
-        <?php include ("assets/templates/navbar.html"); ?>
-
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link default-open active" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./team">Team</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/subtoemail">Subscribe to email marketing</a>
-                    </li>                
-                </ul>
-            </div>
+        <!-- Navigation -->
+        <nav class="w3-bar w3-teal" style="border: 1px solid #ffe41c; border-bottom: none;">
+            <a href="#" class="w3-button w3-bar-item">About</a>
+            <a href="/jumpbound" class="w3-button w3-bar-item">JumpBound</a>
+            <a href="/team" class="w3-button w3-bar-item">Team</a>
+            <a href="/subtoemail" class="w3-button w3-bar-item">Subscribe to email marketing</a>
+            <a href="/contact" class="w3-button w3-bar-item">Contact</a>
         </nav>
-
-        <div class="container" style="margin-top: 30px;">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1><?php echo $digitalTealConfig["aboutHeader"]; ?></h1>
-                    <p><?php echo $digitalTealConfig["about"]; ?></p>
-                    <ul class="nav nav-pills flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#info" onclick="openTab(event, 'info');" id="defaultOpen">Info</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#jumpbound" onclick="openTab(event, 'jumpbound');" id="jumpboundOpen">JumpBound</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#updates" onclick="openTab(event, 'updates');" id="updatesOpen">Latest updates</a>
-                        </li>
-                    </ul>
-                    <hr class="d-sm-none" />
-                </div>
-                <div class="col-sm-8 tabcontent" id="info" style="display: block;">
+        <!-- Banner -->
+        <img
+            src="https://i.ibb.co/7bmKnqd/Digital-Teal-Banner.jpg"
+            alt="The DigitalTeal banner. Contains the brand name DigitalTeal on a purple background."
+            title="The DigitalTeal banner. Contains the brand name DigitalTeal on a purple background."
+            class="banner"
+            style="width: 100%; border: 1px solid #ffe41c;"
+        />
+        <!-- Description -->
+        <section id="description" class="w3-container w3-center w3-content" style="max-width: 600px;">
+            <h2 class="w3-wide">DigitalTeal</h2>
+            <p class="w3-opacity"><i>DigitalTeal is a small coding group</i></p>
+            <p class="w3-justify">
+                 <div class="col-sm-8 tabcontent" id="info" style="display: block;">
                     <noscript>JavaScript is required for this page to display properly.</noscript>
                     <h2>General information</h2>
                     <?php echo $digitalTealConfig["about"]; ?>
@@ -95,11 +73,16 @@ require ("assets/templates/vars.php");
                     <a class="twitter-timeline" data-width="300" data-height="300" data-dnt="true" href="https://twitter.com/digital_teal?ref_src=twsrc%5Etfw">Tweets by digital_teal</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     <div class="fb-page" data-href="https://www.facebook.com/digitalteal" data-tabs="timeline" data-width="300" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/digitalteal" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/digitalteal">DigitalTeal</a></blockquote></div>
                 </div>
-            </div>
-        </div>
+            </p>
+        </section>
+        <!-- Footer -->
+        <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+            <a href="#"><i class="fa fa-facebook-official"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+        </footer>
         
-        <?php include ("assets/templates/footer.html"); ?>
-
+        
         <script>
             // code based on https://w3schools.com/howto/tryit.asp?filename=tryhow_js_vertical_tabs
             function openTab(evt, tabName) {
