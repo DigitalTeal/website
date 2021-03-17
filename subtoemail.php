628ea5990fcb2aa80e0c25369c9ff6c886c60251
ChickenOnYT <dck.dachickenking@gmail.com>
2021-03-17 13:31:48 -0700

    Update subtoemail.php

--- subtoemail.php
+++ subtoemail.php
@@ -8,8 +8,8 @@ include 'assets/templates/vars.php';
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/w3css/3/w3.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
-    <title>DigitalTeal - Home page</title>
-    <meta name="description" content="DigitalTeal is a coding group consisting of 5 members. Currently, DigitalTeal is developing a new and open-source game called JumpBound." />
+    <title>DigitalTeal - Subscribe to email marketing</title>
+    <meta name="description" content="<?php echo config["metaDescription"] ?>" />
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="canonical" href="https://www.digitalteal.com/subtoemail" />
