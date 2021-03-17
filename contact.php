89f7446e5d63a8be04b12b551fdad1c6af6fab98
irhrhd <62729648+irhrhd@users.noreply.github.com>
2021-03-17 13:56:31 -0700

    Added everything

--- contact.php
+++ contact.php
@@ -32,7 +32,78 @@ include 'assets/templates/vars.php';
     <link rel="stylesheet" href="/assets/css/style.css" />
 
     <?php include("assets/templates/analyticsCode.html"); ?>
-  </head>
-  <body>
-  </body>
-</html>
+      <body>
+        <!-- Navigation -->
+        <nav class="w3-bar w3-teal" style="border: 1px solid #ffe41c; border-bottom: none;">
+            <a href="/" class="w3-button w3-bar-item">About</a>
+            <a href="#" class="w3-button w3-bar-item">JumpBound</a>
+            <a href="/team" class="w3-button w3-bar-item">Team</a>
+            <a href="/subtoemail" class="w3-button w3-bar-item">Subscribe to email marketing</a>
+            <a href="/contact" class="w3-button w3-bar-item">Contact</a>
+        </nav>
+        <!-- Banner -->
+        <img
+            src="https://i.ibb.co/7bmKnqd/Digital-Teal-Banner.jpg"
+            alt="The DigitalTeal banner. Contains the brand name DigitalTeal on a purple background."
+            title="The DigitalTeal banner. Contains the brand name DigitalTeal on a purple background."
+            class="banner"
+            style="width: 100%; border: 1px solid #ffe41c;"
+        />
+        <!-- Description -->
+        <section id="description" class="w3-container w3-center w3-content" style="max-width: 600px;">
+            <div class="container" style="margin-top: 30px;">
+                <div class="row">
+                    <h2>DigitalTeal - Contact</h2>
+                    <div class="col-sm-4">
+                        <h1><?php echo $digitalTealConfig["aboutHeader"]; ?></h1>
+                        <p><?php echo $digitalTealConfig["about"]; ?></p>
+                        <ul class="nav nav-pills flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#info" onclick="openTab(event, 'info');" id="defaultOpen">Subscribe to email marketing</a>
+                            </li>
+                        </ul>
+                        <hr class="d-sm-none" />
+                    </div>
+                    <div class="col-sm-8 tabcontent" id="info" style="display: block;">
+                        <noscript>JavaScript is required for this page to display properly.</noscript>
+
+                        <p>Use this page to contact us about questions, complaints, and unsubscribing from emails.</p>
+
+                       <!--  put the new link in here <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdRRsKOOtm6CN3dqBzEV3WJ7pjWglb8rhTJY6wxhJjDmdG-tw/viewform?embedded=true" width="640" height="415" frameborder="0" marginheight="0" marginwidth="0"> -->
+                            Loading…
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer -->
+        <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+            <a href="https://facebook.com/DigitalTeal"><i class="fa fa-facebook-official"></i></a>
+            <a href="https://instagram.com/digital_teal"><i class="fa fa-instagram"></i></a>
+            <a href="https://twitter.com/digital_teal"><i class="fa fa-twitter"></i></a>
+        </footer>
+
+        <script>
+            // code based on https://w3schools.com/howto/tryit.asp?filename=tryhow_js_vertical_tabs
+            function openTab(evt, tabName) {
+                var i, tabcontent, tablinks;
+                tabcontent = document.getElementsByClassName("tabcontent");
+
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+
+                tablinks = document.querySelectorAll(".nav-pills > li.nav-item > a");
+
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+
+                document.getElementById(tabName).style.display = "block";
+                evt.currentTarget.className += " active";
+            }
+            
+            document.getElementById("defaultOpen").click();
+        </script>
+    </body>
+</html>   
