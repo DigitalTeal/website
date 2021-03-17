09238cb925335e2564525d3bad1d0ebf71288e97
ChickenOnYT <dck.dachickenking@gmail.com>
2021-03-17 14:45:14 -0700

    fix the meta desc

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ include 'assets/templates/vars.php';
     <link rel="stylesheet" href="/w3css/3/w3.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
     <title>DigitalTeal - Contact</title>
-    <meta name="description" content=" <?php echo config["metaDescription"] ?> " />
+    <meta name="description" content="<?php echo $digitalTealConfig["metaDescription"] ?>"/>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="canonical" href="https://www.digitalteal.com/contact" />
